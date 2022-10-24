17e3b8ce39ba813767977a01e266aa1326f82772
TanhuKowski <100094931+TanhuKowski@users.noreply.github.com>
2022-10-25 00:56:29 +0400

    Add files via upload

---
+++ Task.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+echo -n "Enter the time: "
+read time
+case $time in
+ "12")
+ echo -n "Time is 12 AM"
+ ;;
+  "13")
+ echo -n "Time is 1 AM"
+ ;;
+  "14")
+ echo -n "Time is 2 AM"
+ ;;
+  "15")
+ echo -n "Time is 3 AM"
+ ;;
+  "16")
+ echo -n "Time is 4 AM"
+ ;;
+  "17")
+ echo -n "Time is 5 AM"
+ ;;
+  "18")
+ echo -n "Time is 6 AM"
+ ;;
+  "19")
+ echo -n "Time is 7 AM"
+ ;;
+  "20")
+ echo -n "Time is 8 AM"
+ ;;
+  "21")
+ echo -n "Time is 9 AM"
+ ;;
+  "22")
+ echo -n "Time is 10 AM"
+ ;;
+  "23")
+ echo -n "Time is 11 AM"
+ ;;
+  "24")
+ echo -n "Time is 12 PM"
+ ;;
+  "1")
+ echo -n "Time is 1 PM"
+ ;;
+  "2")
+ echo -n "Time is 2 PM"
+ ;;
+  "3")
+ echo -n "Time is 3 PM"
+ ;;
+  "4")
+ echo -n "Time is 4 PM"
+ ;;
+  "5")
+ echo -n "Time is 5 PM"
+ ;;
+  "6")
+ echo -n "Time is 6 PM"
+ ;;
+  "7")
+ echo -n "Time is 7 PM"
+ ;;
+  "8")
+ echo -n "Time is 8 PM"
+ ;;
+  "9")
+ echo -n "Time is 9 PM"
+ ;;
+  "10")
+ echo -n "Time is 10 PM"
+ ;;
+  "11")
+ echo -n "Time is 11 PM"
+ ;;
+ esac
+ 
+ 
